619c81ed22ea51be1a9711762c3299de498850b0
allenz3 <allenz3@uw.edu>
2020-05-19 17:57:50 -0700

    aggregate table script

---
+++ scripts/aggregate table script.r
@@ -0,0 +1,32 @@
+## Aggregate Table Script
+
+dataset <- read.csv("final-project-SSGurajala/data/neoplasm/IHME-GBD_2017_DATA-f3575e9c-1.csv", stringsAsFactors=FALSE)
+
+#finds average YYLs per country by percent (1990-2017)
+
+aggregate <- function(dataset) {
+  YYLs <- dataset %>%
+    group_by(location) %>%
+    filter(measure == "YLLs (Years of Life Lost)" 
+           & metric == "Percent") %>%
+    summarise(mean_YYLs = mean(val))
+  
+  Deaths <- dataset %>%
+    group_by(location) %>%
+    filter(measure == "Deaths" 
+           & metric == "Percent") %>%
+    summarise(mean_Deaths = mean(val))
+  
+  DALYs <- dataset %>%
+    group_by(location) %>%
+    filter(measure == "DALYs (Disability-Adjusted Life Years)" 
+           & metric == "Percent") %>%
+    summarise(mean_DALYs = mean(val))
+  
+  table <- left_join(YYLs, Deaths) %>%
+    left_join(DALYs)
+  kable(table, col.names = c("Location", "Years of Life Lost", "Deaths", "Disability-Adjusted Life Years"))
+}
+
+aggregate(dataset)
+
