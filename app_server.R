66dab6e2075d45cc36f3f07a507d13b6f1a9046d
sgura99@uw.edu <sgura99@uw.edu>
2020-06-02 16:31:59 -0700

    read.csv(datasets)

--- app_server.R
+++ app_server.R
@@ -3,12 +3,20 @@ library(shiny)
 library(plotly)
 
 # Read in data
-dataset <- read.csv("data/neoplasm/neoplasms_data.csv",
+neoplasm <- read.csv("data/neoplasm/neoplasms_data.csv",
                     stringsAsFactors = FALSE)
-
+cardiovascular <- read.csv("data/cardiovascular disease/
+                           cardiovascular_disease_data.csv",
+           stringsAsFactors = FALSE)
+chronic_respiratory <- read.csv("data/chronic respiratory disease/
+                                chronic_respiratory_disease_data.csv",
+                    stringsAsFactors = FALSE)
 # Define server function
 server <- function(input, output) {
-  output$summary <- renderTable({ 
+  output$summary <- renderTable( {
     return()
   })
 }
+
+
+
